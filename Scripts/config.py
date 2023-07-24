3becc2bbf00f6c33b987b7b200a8e53ceb9f070c
iluengas <isaacluengas123@gmail.com>
2023-07-23 23:13:53 -0700

    added a git ignore

---
+++ Scripts/config.py
@@ -0,0 +1,6 @@
+
+""" This file seperates api key information from the get_weather script for better maintainability 
+    and this avoids hardcoding the api key into the script.
+"""
+# API Key
+API_KEY = "3f4e2e8c6a20383fe257ece36e096c56"
