b8630ab2356ac2ab8377c618f62edd94db05462d
iluengas <isaacluengas123@gmail.com>
2023-07-23 23:10:15 -0700

    Remove config.py from tracking and add it to .gitignore

--- Scripts/config.py
+++
@@ -1,6 +0,0 @@
-
-""" This file seperates api key information from the get_weather script for better maintainability 
-    and this avoids hardcoding the api key into the script.
-"""
-# API Key
-API_KEY = "3f4e2e8c6a20383fe257ece36e096c56"
